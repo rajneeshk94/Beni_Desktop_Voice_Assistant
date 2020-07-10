b71678539038327b86062afc8e405fe00343cf64
Rajneesh Khare <rajneeshkhare94@gmail.com>
2020-07-10 14:49:26 +0530

    second commit

--- beni.py
+++ beni.py
@@ -1,11 +1,13 @@
 import pyttsx3
 import datetime
+import speechRecognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[1].id)
 
+#Runs the Voice Assistant
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
@@ -21,7 +23,23 @@ def wish_me():
     else:
         speak('Good Evening!')
 
-    speak("Hey there!, I'm Beni. How may I help you?")                
+    speak("Hey there!, I'm Beni. How may I help you?")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print('Listening..')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing')
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f'You said: {query}\n') 
+
+    except Exception as e:
+        print('Please speak again!')
+        return 'None'                           
 
 if __name__ == '__main__':
     wish_me()    
